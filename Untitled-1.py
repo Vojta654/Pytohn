90f8e06dcbe2ca6c7d697b14a6483ca36c93117f
Vojta654 <81766580+Vojta654@users.noreply.github.com>
2022-10-30 20:01:42 +0100

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,23 @@
+from cgitb import text
+
+
+print("Zadejte první číslo:")
+cis1 = int(input())
+text = str(cis1)
+while True:
+    print("Zadejte zanménko:")
+    zn = input()
+    print("Zadejte druhé číslo:")
+    cis2 = int(input())
+    if zn== "+" :
+        vys = cis1 + cis2
+    elif zn== "-" :
+        vys = cis1 - cis2
+    elif zn== "*" :
+            vys = cis1 * cis2   
+    elif zn== "/" :
+            vys = cis1 / cis2
+    
+    text = f'({text}){zn} {cis2} '
+    print(f'{text} = {vys}')
+    cis1 = vys
