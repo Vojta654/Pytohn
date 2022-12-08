14f660ced2068c8dbcf38258f1c3ea6c9103dbd2
Vojta654 <81766580+Vojta654@users.noreply.github.com>
2022-12-08 21:36:11 +0100

    Update 13codes_B.py
    dodělány všechny kontroly a FUNGUJE TO!!

--- 13codes_B.py
+++ 13codes_B.py
@@ -1,7 +1,32 @@
-# šifrování
-# sifrovani = input("Chcete šifrovat(E), nebo dešifrovat(D)?")
+while True:  # konto
+    sifrovani = input("Chcete šifrovat(E), nebo dešifrovat(D)?")
+    if sifrovani == "E" or sifrovani == "D":
+        break
+
 sentence = input("Zadjte větu: ")
-key = input("Napište klíč: ")
+
+
+while True:
+    key = input("Napište klíč: ")
+    key_index = 0
+    while key_index < len(key):
+        key_znak = ord(key[key_index])
+        if key_znak < 123 and key_znak > 64:
+            if key_znak > 90 and key_znak < 97:
+                print("byl zadán neplatný klíč používejte pouze písemna")
+                working = False
+                break
+            else:
+                working = True
+                key_index += 1
+        else:
+            working = False
+            break
+    if working == False:
+        continue
+    elif working == True:
+        break
+
 key_index = 0
 index = 0
 while index < len(sentence):
@@ -11,23 +36,58 @@ while index < len(sentence):
         num_znak = 0  # mezera
         print(" ", end="")
         index += 1
-
         continue
-    # print(index)
-    if num_znak > 96:  # malé píseno
-        num_klic = ord(key[key_index]) - 97
-        if num_klic + num_znak > 122:
-            print(chr(num_znak - 26 + num_klic), end="")
-        else:
-            print(chr(num_znak + num_klic), end="")
+    num_klic = ord(key[key_index])
+    if num_klic > 96 and num_klic < 123:  # převod klíče na pořadí v abecedě
+        num_klic -= 97  # pro velké písmeno v klíči
+    elif num_klic < 64 and num_klic > 91:
+        num_klic -= 65  # pro malé písmeno v klíči
 
-    if num_znak < 96:  # velké píseno
-        num_klic = ord(key[key_index]) - 65
-        if num_klic + num_znak > 90:
-            print(chr(num_znak - 26 + num_klic), end="")
-        else:
-            print(chr(num_znak + num_klic), end="")
+    if sifrovani == "E":  # šifrovování
+
+        if num_znak > 96 and num_znak < 123:  # malé píseno
+            if num_klic + num_znak > 122:
+                print(chr(num_znak - 26 + num_klic), end="")
+            else:
+                print(chr(num_znak + num_klic), end="")
+
+        if num_znak > 65 and num_znak < 91:  # velké píseno
+            if num_klic + num_znak > 90:
+                print(chr(num_znak - 26 + num_klic), end="")
+            else:
+                print(chr(num_znak + num_klic), end="")
+        if num_znak < 65 or num_znak > 122:  # není písmeno
+            print(sentence[index], end="")
+            index += 1
+            continue
+        if num_znak > 90 and num_znak < 97:  # není písmeno
+            print(sentence[index], end="")
+            index += 1
+            continue
+
+    if sifrovani == "D":  # dešifrovování
+
+        if num_znak > 96 and num_znak < 123:  # malé píseno
+            if num_znak - num_klic < 97:
+                print(chr(num_znak + 26 - num_klic), end="")
+            else:
+                print(chr(num_znak - num_klic), end="")
+
+        if num_znak > 65 and num_znak < 91:  # velké píseno
+            if num_znak - num_klic < 65:
+                print(chr(num_znak + 26 - num_klic), end="")
+            else:
+                print(chr(num_znak - num_klic), end="")
+
+        if num_znak < 65 or num_znak > 122:  # není písmeno
+            print(sentence[index], end="")
+            index += 1
+            continue
+        if num_znak > 90 and num_znak < 97:  # není písmeno
+            print(sentence[index], end="")
+            index += 1
+            continue
     index += 1
     key_index += 1
-    if key_index == len(key):
+    if key_index == len(key):  # když klíč dojde do konce
         key_index = 0
