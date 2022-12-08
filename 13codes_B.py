2c0bc2b4b492ed6fb5fb37b773f9bdb68e10c31b
Vojta654 <81766580+Vojta654@users.noreply.github.com>
2022-12-08 19:10:05 +0100

    Update 13codes_B.py

--- 13codes_B.py
+++ 13codes_B.py
@@ -1,32 +1,33 @@
 # šifrování
-sifrovani = input("Chcete šifrovat(E), nebo dešifrovat(D)?")
-sentence = input("Zadjte větu ")
-key = input("Napište klíč")
-key_index =0
+# sifrovani = input("Chcete šifrovat(E), nebo dešifrovat(D)?")
+sentence = input("Zadjte větu: ")
+key = input("Napište klíč: ")
+key_index = 0
 index = 0
 while index < len(sentence):
     num_znak = ord(sentence[index])
 
     if num_znak == 32:
-        num_znak = 0 # mezera
+        num_znak = 0  # mezera
         print(" ", end="")
-        index +=1
+        index += 1
+
         continue
-    if num_znak >96:  #malé píseno
-        num_klic = ord(key[index]) - 97
-        if num_klic + num_znak - 96 > 0:
-            print(chr(num_znak+num_klic), end="")
+    # print(index)
+    if num_znak > 96:  # malé píseno
+        num_klic = ord(key[key_index]) - 97
+        if num_klic + num_znak > 122:
+            print(chr(num_znak - 26 + num_klic), end="")
         else:
-            print(chr(num_znak -26 + num_klic), end="")
+            print(chr(num_znak + num_klic), end="")
 
-    if num_znak <96:  #velké píseno
-        num_klic = ord(key[index]) - 65
-        if num_klic + num_znak - 65 > 0:
-            print(chr(num_znak+num_klic), end="")
+    if num_znak < 96:  # velké píseno
+        num_klic = ord(key[key_index]) - 65
+        if num_klic + num_znak > 90:
+            print(chr(num_znak - 26 + num_klic), end="")
         else:
-            print(chr(num_znak -26 + num_klic), end="")
-    if index == len(key):
-        key_index =0
-    index +=1
-    key_index +=1
-
+            print(chr(num_znak + num_klic), end="")
+    index += 1
+    key_index += 1
+    if key_index == len(key):
+        key_index = 0
